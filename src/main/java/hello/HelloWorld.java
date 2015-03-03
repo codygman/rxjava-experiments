f7b9118b3f70441a291fbe4330f1d93f94432062
Cody Goodman <codygman.consulting@gmail.com>
2015-03-03 00:52:43 -0600

    pointless code golf 2

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -8,14 +8,12 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-	Action1<String> onNextAction = new Action1<String>() {
-	    @Override
-	    public void call(String s) {
-		System.out.println(s);
-	    }
-	};
-    
-        Observable.just("Hello, world!").subscribe(onNextAction);
-
+        Observable.just("Hello, world!").subscribe(
+            new Action1<String>() {
+	        @Override
+	        public void call(String s) {
+		    System.out.println(s);
+	        }
+        });
     }
 }
