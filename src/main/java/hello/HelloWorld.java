d9b4188692f7e38997c144ac503bbf23d5d4cc43
Cody Goodman <codygman.consulting@gmail.com>
2015-03-03 00:29:07 -0600

    Add rxjava tutorials example

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,9 +1,24 @@
 package hello;
 
 import org.joda.time.LocalTime;
+import rx.Observable;
+import rx.functions.Action1;
 
 public class HelloWorld {
+
+    public static void hello(String... names) {
+	Observable.from(names).subscribe(new Action1<String>() {
+		@Override
+		public void call(String s) {
+		    System.out.println("Hello " + s + "!");
+		}
+	});
+    }
+
+    
     public static void main(String[] args) {
+	hello("Cody", "Amia");
+	
 	LocalTime currentTime = new LocalTime();
 	System.out.println("The current local time is: " + currentTime);
 	
