f351724cd9908d38000df690b596b919dfb01040
Cody Goodman <codygman.consulting@gmail.com>
2015-03-03 00:40:55 -0600

    Working first version of tutorial

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,11 +1,35 @@
 package hello;
 
 import rx.Observable;
+import rx.Subscriber;
 import rx.functions.Action1;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-	System.out.println("Hello world!");
+
+        Observable<String> myObservable = Observable.create(
+        	new Observable.OnSubscribe<String>() {
+        	    @Override
+        	    public void call(Subscriber<? super String> sub) {
+        		sub.onNext("Hello, world!");
+        		sub.onCompleted();
+        	    }
+		}
+        );
+    
+        Subscriber<String> mySubscriber = new Subscriber<String>() {
+	    @Override
+	    public void onNext(String s) { System.out.println(s);}
+    
+	    @Override
+	    public void onCompleted() {}
+    
+	    @Override
+	    public void onError(Throwable e) {}
+        };
+    
+        myObservable.subscribe(mySubscriber);
+
     }
 }
