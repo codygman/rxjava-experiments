5ba7f08726506c62fa5653833c106c9061e377ef
Cody Goodman <codygman.consulting@gmail.com>
2015-03-03 00:31:43 -0600

    remove unnecessary stuff

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,28 +1,11 @@
 package hello;
 
-import org.joda.time.LocalTime;
 import rx.Observable;
 import rx.functions.Action1;
 
 public class HelloWorld {
 
-    public static void hello(String... names) {
-	Observable.from(names).subscribe(new Action1<String>() {
-		@Override
-		public void call(String s) {
-		    System.out.println("Hello " + s + "!");
-		}
-	});
-    }
-
-    
     public static void main(String[] args) {
-	hello("Cody", "Amia");
-	
-	LocalTime currentTime = new LocalTime();
-	System.out.println("The current local time is: " + currentTime);
-	
-	Greeter greeter = new Greeter();
-	System.out.println(greeter.sayHello());
+	System.out.println("Hello world!");
     }
 }
