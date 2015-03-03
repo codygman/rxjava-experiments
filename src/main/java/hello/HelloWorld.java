f6d9e617bb8ba34539f0ff98459864c2acbe0ec5
Cody Goodman <codygman.consulting@gmail.com>
2015-03-03 00:42:19 -0600

    use Observable.just

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -8,15 +8,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        Observable<String> myObservable = Observable.create(
-        	new Observable.OnSubscribe<String>() {
-        	    @Override
-        	    public void call(Subscriber<? super String> sub) {
-        		sub.onNext("Hello, world!");
-        		sub.onCompleted();
-        	    }
-		}
-        );
+        Observable<String> myObservable = Observable.just("Hello, world!");
     
         Subscriber<String> mySubscriber = new Subscriber<String>() {
 	    @Override
