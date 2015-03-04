adc18bcc3130f6a39343f27d8bbdd281ac553cad
Cody Goodman <codygman.consulting@gmail.com>
2015-03-03 23:39:04 -0600

    Append my name

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,12 +3,20 @@ package hello;
 import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
 
-        Observable.just("Hello, world!").subscribe(
+        Observable.just("Hello, world!")
+	    .map(new Func1<String, String>() {
+		    @Override
+		    public String call(String s) {
+			return s + " -Cody";
+		    }
+		})
+	    .subscribe(
             new Action1<String>() {
 	        @Override
 	        public void call(String s) {
